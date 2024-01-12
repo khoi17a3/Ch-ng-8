e60f5bc94411e68b8f332a7663301ee712343b16
Nguyễn Trọng Khôi- DHKL17a3HN_19_0146 <148676816+khoi17a3@users.noreply.github.com>
2024-01-12 11:34:34 +0700

    Update 8.8.py

--- 8.8.py
+++ 8.8.py
@@ -1,17 +1,39 @@
-a=eval(input("Nhập số KWh tiêu thụ:"))
-if a>=0:
-    if a<=50:
-       print("Tổng số tiền phải trả là:",a*1678,"đồng.")
-    elif a<=100:
-       print("Tổng số tiền phải trả là:",50*1678+(a-50)*1734,"đồng.")
-    elif a<=200:
-       print("Tổng số tiền phải trả là:",50*(1678+1734)+(a-100)*2014,"đồng.")
-    elif a<=300:
-       print("Tổng số tiền phải trả là:",50*(1678+1734+2014)+(a-200)*2536,"đồng.")
-    elif a<=400:
-       print("Tổng số tiền phải trả là:",50*(1678+1734+2014+2536)+(a-300)*2834,"đồng.")
-    else:
-       print("Tổng số tiền phải trả là:",50*(1678+1734+2014+2536+2834)+(a-400)*2927,"đồng.")     
+print("Các mã loại phòng cho bạn:")
+print("1-Standard")
+print("2-Superior Garden View")
+print("3-Superior Ocean View")
+print("4-Garden View Bungalow")
+print("5-Pool View Bungalow")
+print("6-Family Room")
+print("7-Beach Front Bungalow")
+print("8-VIP sea View")
+
+a = int(input("Nhập mã loại phòng:"))
+b = int(input("Nhập số đêm:"))
+
+if a == 1:
+    c = 1260000
+elif a == 2:
+    c = 1550000
+elif a == 3 or a == 4:
+    c = 1830000
+elif a == 5 or a == 6:
+    c = 2120000
+elif a == 7:
+    c = 2540000
+elif a == 8:
+    c = 4800000
 else:
-   print("Số KWh không hợp lệ.")
+    print("Vui lòng chọn lại mã loại phòng.")
+    c = 0
+
+if c != 0:
+    if b == 1:
+        print("Giá tiền phải trả là:", c, "đồng.")
+    elif b in [2, 3]:
+        print("Giá tiền phải trả là:", c * b * 0.75, "đồng.")
+    elif b >= 4:
+        print("Giá tiền phải trả là:", c * b * 0.7, "đồng.")
+    else:
+        print("Vui lòng nhập lại số đêm.")
    
